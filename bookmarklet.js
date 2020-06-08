81346a6840fa6d022cd6ee5f62f4ae1e8e9d63f7
Samrat Mallick <62864554+coolaquas@users.noreply.github.com>
2020-06-08 21:09:31 +0530

    Update bookmarklet.js

--- bookmarklet.js
+++ bookmarklet.js
@@ -1,7 +1,7 @@
 (()=>{
     let paragraphs= document.getElementsByTagName('p');
     for (let i=0; i < paragraphs.length;i++){
-        paragraphs[i].innerHTML = "12345";
+        paragraphs[i].innerHTML = "";
         
     }
 })();   
