ae0a35ed8576a01b5b46da3f2ede104432b730e5
Samrat Mallick <62864554+coolaquas@users.noreply.github.com>
2020-06-08 20:48:03 +0530

    Add files via upload

---
+++ bookmarklet.js
@@ -0,0 +1,9 @@
+(()=>{
+    let paragraphs= document.getElementsByTagName('p');
+    for (let i=0; i < paragraphs.length;i++){
+        paragraphs[i].innerHTML = "12345";
+        
+    }
+})();   
+
+
